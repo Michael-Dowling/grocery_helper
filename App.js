e8213117e6c1d0171f505b2e2eb7d75ead3119d1
Michael Dowling <15mhd@queensu.ca>
2018-12-01 17:11:27 -0500

    added database functionality

--- App.js
+++ App.js
@@ -1,21 +1,157 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+import Dialog from 'react-native-dialog';
+import DialogContainer from 'react-native-dialog/src/Container';
+const firebase = require('firebase');
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js')
 
-export default class App extends React.Component {
+const {
+  AppRegistry,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Alert,
+} = ReactNative;
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBYg-JdPbI8vUabkyB92s42RlBs9bRhlH4",
+  authDomain: "groceryhelper-d8975.firebaseapp.com",
+  databaseURL: "https://groceryhelper-d8975.firebaseio.com",
+  storageBucket: "groceryhelper-d8975.appspot.com",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default class FirebaseReactNativeSample extends Component {
+  constructor(props) {
+    super(props);
+    this.itemsRef = this.getRef().child('items');
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      dialogVisible : false,
+      quanDialogVisible : false
+    };
+    this.setState({dialogVisible: true})
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val()['grocery'] + " - " + child.val()['quantity'],
+          _key: child.key
+        });
+      });
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+  handleCancel = () => {
+    this.setState({ dialogVisible: false });
+  };
+  handleAdd = () => {
+    console.log(this.state.grocery);
+    var grocery = this.state.grocery;
+    var quantity = 2;
+    //this.state.quantity = 2;
+    this.itemsRef.push({
+      grocery,
+      quantity
+    });
+    this.setState({dialogVisible: false})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+        <StatusBar title="Food at Home" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+        <Dialog.Container visible={this.state.dialogVisible}>
+          <Dialog.Title>Add Groceries</Dialog.Title>
+          <Dialog.Button label="Cancel" onPress={this.handleCancel}/>
+          <Dialog.Button label="Add" onPress={this.getQuantity}/>
+          <Dialog.Input label="Type below:"
+            value={this.state.grocery}
+            onChangeText={grocery => this.setState({grocery})}>
+          </Dialog.Input>
+        </Dialog.Container>
+        <Dialog.Container visible={this.state.quanDialogVisible}>
+          <Dialog.Title>Add Groceries</Dialog.Title>
+          <Dialog.Button label="Cancel" onPress={this.handleCancel}/>
+          <Dialog.Button label="Add" onPress={this.handleAdd}/>
+          <Dialog.Input label="Type below:"
+            value={this.state.quantity}
+            onChangeText={quantity => this.setState({quantity})}>
+          </Dialog.Input>
+        </Dialog.Container>
       </View>
+    )
+  }
+
+  _addItem() {
+    this.setState({dialogVisible: true})
+
+    // Alert.alert(
+    //   'Add Item',
+    //   null,
+    //   [
+    //     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+    //     {
+    //       text: 'Add',
+    //       onPress: (text) => {
+    //         this.itemsRef.push({ title: text })
+    //       }
+    //     },
+    //   ],
+    //   'plain-text'
+    // );
+  }
+
+  _renderItem(item) {
+
+    const onPress = () => {
+      Alert.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
     );
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('FirebaseReactNativeSample', () => FirebaseReactNativeSample);
